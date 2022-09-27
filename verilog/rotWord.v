31ffb3f2dc08164d5ca9644c241a39dc00fb8c6e
mavioux <pakmarios@gmail.com>
2022-09-28 01:28:21 +0300

    Unclock RotWord

--- verilog/rotWord.v
+++ verilog/rotWord.v
@@ -1,10 +1,8 @@
 module rotWord (
-    input wire clk,
-    input wire reset,
     input wire [0:31] w_in,
     output reg [0:31] w_out
 );
-    always  @ (posedge clk ) begin
+    always  @ (w_in [0:31]) begin
         w_out[0:7] <= w_in[8:15];
         w_out[8:15] <= w_in[16:23];
         w_out[16:23] <= w_in[24:31];
